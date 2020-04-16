9346caec1bc9da46af1b03926c5431c8f400074a
Brady Klein <brady.klein@huskers.unl.edu>
2020-04-15 23:40:07 -0500

    Began work on styling website and basic layout

---
+++ components/Header.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <style jsx global>{`
+        .header ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: #333;
+          position: fixed;
+          top: 0;
+          width: 100%;
+        }
+
+        .header li {
+          float: left;
+        }
+
+        .header li a {
+          display: block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+
+        /* Change the link color to #111 (black) on hover */
+        .header li a:hover {
+          background-color: #111;
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import MainLayout from "../components/MainLayout";
+
 export default function Index() {
-    return (
-        <div>
-            <p>Hello Next.js</p>
-        </div>
-    );
+  return (
+    <>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <div>
+        <MainLayout>
+          <p>Hello Next.js</p>
+        </MainLayout>
+      </div>
+      <style jsx>{`
+        div {
+          font-family: "Montserrat", sans-serif;
+          margin-top: 60px;
+        }
+      `}</style>
+    </>
+  );
 }
